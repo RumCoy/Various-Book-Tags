7a0b718aa730dcab2013b4aa388e4789a83164df
RumCoy <309020109+RumCoy@users.noreply.github.com>
2026-08-10 18:26:22 -0700

    Fix SKSE log filename

--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,9 @@
 #include "Menu.h"
 
 #include <SKSE/SKSE.h>
-#include <spdlog/sinks/basic_file_sink.h>
 
 namespace
 {
-    void InitializeLogging()
-    {
-        const auto directory = SKSE::log::log_directory();
-        if (!directory) {
-            SKSE::stl::report_and_fail("Unable to locate the SKSE log directory");
-        }
-
-        auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(
-            (*directory / "VariousBookTags.log").string(), true);
-        auto logger = std::make_shared<spdlog::logger>("global", std::move(sink));
-#ifndef NDEBUG
-        logger->set_level(spdlog::level::debug);
-#else
-        logger->set_level(spdlog::level::info);
-#endif
-        logger->flush_on(spdlog::level::info);
-        spdlog::set_default_logger(std::move(logger));
-    }
-
     void OnSKSEMessage(SKSE::MessagingInterface::Message* message)
     {
         if (!message || message->type != SKSE::MessagingInterface::kDataLoaded) {
@@ -57,7 +37,6 @@ namespace
 
 SKSEPluginLoad(const SKSE::LoadInterface* skse)
 {
-    InitializeLogging();
     SKSE::Init(skse);
 
     auto* messaging = SKSE::GetMessagingInterface();
